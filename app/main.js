3b822026883a112c8ca0a1bbaa3aa01b5aec05d4
gonzalomarcylabschool <gonzalo@marcylabschool.org>
2023-09-15 12:19:57 -0400

    functional

--- app/main.js
+++ app/main.js
@@ -1,18 +1,81 @@
 
 
-async function fetchData(api) {
+const body = document.body;
+const logo = document.createElement('img');
+logo.src = `logo.png`
+logo.className = 'logoImage'
+
+
+body.append(logo);
+const getAllPokemon = async () => {
   try {
-    const response = await fetch(api);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+    // Step 1: Perform the initial fetch to get an array of data
+    const allResponse = await fetch('https://pokeapi.co/api/v2/pokemon/');
+    const allData = await allResponse.json();
+    
+
+    const fetchPokeData = async (item) => {
+      const pokeDataResponse = await fetch(item);
+      const pokeData = await pokeDataResponse.json();
+      console.log(pokeData);
+      const pokeBox = document.createElement('div');
+      const name = document.createElement('h2');
+      const sprite = document.createElement('img');
+      const button = document.createElement('input');
+      name.innerText = pokeData.name;
+      sprite.src = `${pokeData.sprites.other['official-artwork']['front_default']}`;
+      sprite.alt = `picture of ${name}`;
+      button.value = `More info`;
+      button.type = 'submit'
+      button.id= `${pokeData.name}Button`
+      pokeBox.append(name, sprite, button );
+      // const moreInfo = body.querySelector(`#${pokeData.name}Button`)
+      
+      
+      const card = document.createElement('div');
+      card.className = `${pokeData.name}Card`;
+      card.textContent = `${pokeData.name} is a pokemon that can be found in the Following version of the Pokemon games:`;
+
+      card.style.display = 'none'; // Initially hide the card
+      
+      body.append(card)
+      button.addEventListener('click', e => {
+        e.preventDefault()
+        const cardClass = document.getElementsByClassName(`${pokeData.name}Card`)
+        if(e.target.id === `${pokeData.name}Button`){
+          if (card.style.display === 'none') {
+            card.style.display = 'block';
+            
+            pokeData.game_indices.forEach(async (item) => {
+              console.log(item.version.name);
+              const game = document.createElement('p')
+              game.textContent = item.version.name;
+              card.append(game);
+            })
+            console.log(pokeData.game_indices);
+            card.append(name, button, sprite);
+            
+            
+            
+          } else {
+            card.style.display = 'none';
+            pokeBox.append(name, sprite, button );
+          };
+        };
+      });
+      body.appendChild(pokeBox);
+      
+      
     }
-    const data = await response.json();
-    console.log(data)
+    allData.results.forEach(async (item) => {
+      await fetchPokeData(item.url);
+    });
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error('Error:', error);
   }
 }
 
-fetchData('https://pokeapi.co/api/v2/pokemon/');
 
-fetchData('https://pokeapi.co/api/v2/pokemon/1');
+
+
+getAllPokemon();
